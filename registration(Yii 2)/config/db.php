1ac961b58c4c240ffdf38e4e4583353bf4becb87
M.Usikov <usikov@topnlab.ru>
2020-05-13 22:16:09 +0300

    ..

--- registration(Yii 2)/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => 'pAsS61208465783lh',
-    'charset' => 'utf8',
-];
